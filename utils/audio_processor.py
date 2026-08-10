95c59f7618b072accb3aede8bbae920b52d9eb02
KOLAJHANSI <jhansiiikola@gmail.com>
2026-08-10 13:12:19 +0530

    .

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -5,11 +5,28 @@ import os
 DOWNLOAD_DIR = "downloades"
 os.makedirs(DOWNLOAD_DIR,exist_ok = True)
 
-def download_youtube_audio(url : str) -> str:
+def download_youtube_audio(url: str) -> str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+
     ydl_opts = {
         "format": "bestaudio/best",
         "outtmpl": output_path,
+        "noplaylist": True,
+
+        "http_headers": {
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/131.0.0.0 Safari/537.36"
+            ),
+        },
+
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["android", "web"]
+            }
+        },
+
         "postprocessors": [
             {
                 "key": "FFmpegExtractAudio",
@@ -17,11 +34,18 @@ def download_youtube_audio(url : str) -> str:
                 "preferredquality": "192",
             }
         ],
+
         "quiet": True,
     }
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
+        filename = (
+            ydl.prepare_filename(info)
+            .replace(".webm", ".wav")
+            .replace(".m4a", ".wav")
+        )
+
     return filename
 
 def convert_to_wav(input_path: str) -> str:
